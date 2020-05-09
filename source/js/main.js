892f3db233dae2dfe52922abba1fbf73ff64dae1
AoTxLand <aotxland@gmail.com>
2020-05-09 18:03:46 +0800

    fix bugs

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,1 @@
 // Add your script here
-
-import baguetteBox from 'baguettebox.js'
-
-window.addEventListener('load', function() {
-  baguetteBox.run('.post-content .post-img-lb')
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const Webpack = require("webpack");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
@@ -8,36 +8,24 @@ const path = require("path");
 
 const join = (...paths) => path.join(__dirname, ...paths);
 
-const entryPoints = {
-  "main.js": [
-    join("source", "js", "main.js"),
-    join("source", "js", "menu.js"),
-    join("source", "js", "languageSelector.js")
-  ],
-  "prism.js": join("source", "js", "prism.js"),
-}
-
-const postcssStyles = [
-  { name: "style.css", path: join("source", "css", "style.css") },
-  { name: "red.css", path: join("source", "css", "color", "red.css") },
-  { name: "blue.css", path: join("source", "css", "color", "blue.css") },
-  { name: "green.css", path: join("source", "css", "color", "green.css") },
-  { name: "pink.css", path: join("source", "css", "color", "pink.css") },
-]
-
-postcssStyles.forEach(entry => {
-  entryPoints[entry.name] = entry.path;
-});
-
-// console.info("%o", entryPoints)
-
 module.exports = {
-  mode: 'production',
   resolve: {
     extensions: [".js", ".css"],
     modules: ["source", "node_modules"],
   },
-  entry: entryPoints,
+  entry: {
+    "main.js": [
+      join("source", "js", "main.js"),
+      join("source", "js", "menu.js"),
+      join("source", "js", "languageSelector.js")
+    ],
+    "prism.js": join("source", "js", "prism.js"),
+    "style.css": join("source", "css", "style.css"),
+    "red.css": join("source", "css", "color", "red.css"),
+    "blue.css": join("source", "css", "color", "blue.css"),
+    "green.css": join("source", "css", "color", "green.css"),
+    "pink.css": join("source", "css", "color", "pink.css"),
+  },
   output: {
     filename: "[name]",
     path: join("static/assets"),
@@ -71,28 +59,28 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-            options: {
-              minimize: true,
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[local]",
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              config: {
-                path: "postcss.config.js",
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                minimize: true,
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[local]",
               },
             },
-          },
-        ],
+            {
+              loader: "postcss-loader",
+              options: {
+                config: {
+                  path: "postcss.config.js",
+                },
+              },
+            },
+          ],
+        }),
       },
     ],
   },
@@ -107,15 +95,5 @@ module.exports = {
       }),
     ],
   },
-  plugins: [
-    new CleanPlugin(join("static/assets")),
-    new MiniCssExtractPlugin({
-      // since entry filename is also xxx.css, which actully is a js file,
-      // we need to use another extension for extracted css to avoid error
-      moduleFilename: (chunk) => {
-        const name = chunk.name.replace(/.css/, '');
-        return `${name}.min.css`;
-      },
-    })
-  ],
+  plugins: [new CleanPlugin(join("static/assets")), new ExtractTextPlugin("[name]")],
 };
